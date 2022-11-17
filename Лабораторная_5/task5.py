ea46165dd82312d0e3da5562b3a27756aa8c4f5a
rumyantsevaea <115364029+rumyantsevaea@users.noreply.github.com>
2022-11-17 19:01:59 +0300

    Update task5.py

--- Лабораторная_5/task5.py
+++ Лабораторная_5/task5.py
@@ -1,5 +1,9 @@
 from random import sample
-s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklnopqrstuvwxyz1234567890'
+import string
+s1 = list(string.ascii_lowercase)
+s2 = list(string.ascii_uppercase)
+s3 = list(string.digits)
+s = s1 + s2 + s3
 def get_random_password(n) -> str:
     return ''.join(sample(s,n))
 
